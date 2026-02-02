41db62782945c44f87dd577e0253a35449a38446
Ajith <134156010+Ajith-ajay@users.noreply.github.com>
2026-02-02 15:08:14 +0530

    Implemented (#54)
    * Frontend Initial Commit
    
    * Merge remote-tracking branch 'origin/main' into newgen
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' into newgen
    
    * Landing login page
    
    * PWA Registered ....
    
    * Merge branch 'main' into newgen
    
    * Merge branch 'main' into newgen
    
    * Some Changes
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * .exe Done
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * .exe Enhanced
    
    * Merge branch 'main' into newgen
    
    * Merge branch 'main' into newgen
    
    * I think so
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' into newgen
    
    * next question logic fixed
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' into newgen
    
    * Schedule Changes
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Merge branch 'main' into newgen
    
    * Upload all completed
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * ..
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * instruction updated
    
    * Merge branch 'main' of https://github.com/4274-hari/QA-Examination-Platform into newgen
    
    * Login Heading Fix
    
    * Merge branch 'main' into newgen
    
    * No Issue
    
    * duration fix
    
    * Merge branch 'main' into newgen
    
    * Electron Testing
    
    * Logged backend
    
    * Upload finished
    
    * Merge branch 'main' into newgen
    
    * Bunch of question
    
    * Merge branch 'main' into newgen
    
    * Merge branch 'main' into newgen
    
    * Update check
    
    * small changes
    
    * Merge branch 'main' into newgen
    
    * Merge branch 'main' into newgen
    
    * Merge branch 'main' into newgen

--- Backend/controllers/staff_controllers/form_handle_controller.js
+++ Backend/controllers/staff_controllers/form_handle_controller.js
@@ -1,65 +1,52 @@
 const {getDb} = require('../../config/db');
 
 async function addHandleForm(req,res) {
-
     try {
-
         const db = getDb();
-
         const collection = db.collection("qa_form");
-
         const {regulation,academic_year} = req.body;
 
-         if (
-      (!Array.isArray(regulation) || regulation.length === 0) &&
-      (!Array.isArray(academic_year) || academic_year.length === 0)
-    ) {
-      return res.status(400).json({
-        message: "No valid data provided"
-      });
-    }
+        if (
+          (!Array.isArray(regulation) || regulation.length === 0) &&
+          (!Array.isArray(academic_year) || academic_year.length === 0)
+        ) {
+          return res.status(400).json({
+            message: "No valid data provided"
+          });
+        }
 
         if(Array.isArray(regulation) && regulation.length > 0){
-
-              await collection.updateOne(
-        { regulation: { $exists: true } },
-        {
-          $addToSet: {
-            regulation: { $each: regulation }
-          }
-        },
-        { upsert: true }
-      );
-            
+          await collection.updateOne(
+            { regulation: { $exists: true } },
+            {
+              $addToSet: {
+                regulation: { $each: regulation }
+              }
+            },
+            { upsert: true }
+          );
         }
 
         if(Array.isArray(academic_year) && academic_year.length > 0){
-
-            await collection.updateOne(
-        { academic_year: { $exists: true } },
-        {
-          $addToSet: {
-            academic_year: { $each: academic_year }
-          }
-        },
-        { upsert: true }
-      );
-
+          await collection.updateOne(
+            { academic_year: { $exists: true } },
+            {
+              $addToSet: {
+                academic_year: { $each: academic_year }
+              }
+            },
+            { upsert: true }
+          );
         }
 
-         return res.status(200).json({
-      success: true,
-      message: "Added successfully"
-    });
-        
+      return res.status(200).json({
+        success: true,
+        message: "Added successfully"
+      });
     } catch (error) {
-
-        console.error(error);
-
-        return res.status(500).json({success: false,message: "Internal server error"});
-
+      console.error(error);
+      return res.status(500).json({success: false,message: "Internal server error"});
     }
-    
 }
 
 
